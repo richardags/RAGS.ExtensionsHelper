100fb9896040b40d6e9bda0b5536184991392a90
richardags <richardags@hotmail.com>
2023-05-09 20:21:11 +0100

    Add project files.

---
+++ DateTimeOffsetExtension/DateTimeOffsetExtension.cs
@@ -0,0 +1,53 @@
+﻿using DateTimeOffsetExtension.classes;
+using System;
+using TelegramHelper;
+
+namespace DateTimeOffsetExtension
+{
+    public static class DateTimeOffsetExtension
+    {
+        public static DateTimeOffset TruncateMillisecond(this DateTimeOffset dateTimeOffset)
+        {
+            return new DateTimeOffset(
+                dateTimeOffset.Year, dateTimeOffset.Month, dateTimeOffset.Day,
+                dateTimeOffset.Hour, dateTimeOffset.Minute, dateTimeOffset.Second,
+                TimeSpan.Zero);
+        }
+        public static DateTimeOffset TruncateSecond(this DateTimeOffset dateTimeOffset)
+        {
+            return new DateTimeOffset(
+                dateTimeOffset.Year, dateTimeOffset.Month, dateTimeOffset.Day,
+                dateTimeOffset.Hour, dateTimeOffset.Minute, 0,
+                TimeSpan.Zero);
+        }
+
+        public static string FullFormat(this DateTimeOffset dateTimeOffset, TelegramLanguageEnum language)
+        {
+            switch (language)
+            {
+                case TelegramLanguageEnum.ENGLISH:
+                    return dateTimeOffset.ToString(Constants.ENGLISH_FULL_DATETIME_FORMAT);
+                case TelegramLanguageEnum.PORTUGUESE:
+                    return dateTimeOffset.ToString(Constants.PORTUGUESE_FULL_DATETIME_FORMAT);
+                case TelegramLanguageEnum.SPANISH:
+                    return dateTimeOffset.ToString(Constants.SPANISH_FULL_DATETIME_FORMAT);
+                default:
+                    throw new Exception("DateTimeOffsetExtension.cs FullFormat() - unknown language: " + language);
+            }
+        }
+        public static string ShortFormat(this DateTimeOffset dateTimeOffset, TelegramLanguageEnum language)
+        {
+            switch (language)
+            {
+                case TelegramLanguageEnum.ENGLISH:
+                    return dateTimeOffset.ToString(Constants.ENGLISH_SHORT_DATETIME_FORMAT);
+                case TelegramLanguageEnum.PORTUGUESE:
+                    return dateTimeOffset.ToString(Constants.PORTUGUESE_SHORT_DATETIME_FORMAT);
+                case TelegramLanguageEnum.SPANISH:
+                    return dateTimeOffset.ToString(Constants.SPANISH_SHORT_DATETIME_FORMAT);
+                default:
+                    throw new Exception("DateTimeOffsetExtension.cs ShortFormat() - unknown language: " + language);
+            }
+        }
+    }
+}
---
+++ DateTimeOffsetExtension/classes/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace DateTimeOffsetExtension.classes
+{
+    internal class Constants
+    {
+        public static readonly string ENGLISH_FULL_DATETIME_FORMAT = "MM/dd/yyyy HH:mm:ss.FFF K";
+        public static readonly string PORTUGUESE_FULL_DATETIME_FORMAT = "dd/MM/yyyy HH:mm:ss.FFF K";
+        public static readonly string SPANISH_FULL_DATETIME_FORMAT = "dd/MM/yyyy HH:mm:ss.FFF K";
+
+        public static readonly string ENGLISH_SHORT_DATETIME_FORMAT = "MM/dd/yyyy HH:mm:ss K";
+        public static readonly string PORTUGUESE_SHORT_DATETIME_FORMAT = "dd/MM/yyyy HH:mm:ss K";
+        public static readonly string SPANISH_SHORT_DATETIME_FORMAT = "dd/MM/yyyy HH:mm:ss K";
+    }
+}
